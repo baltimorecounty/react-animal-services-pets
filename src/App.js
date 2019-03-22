819da5615c050f67c6b0eaa0a9d90e6d42e62021
Marty Powell <jpowel25@gmail.com>
2019-03-22 16:52:56 -0400

    fixed logic so tabs contain the proper content

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import { petFilter } from "./services/PetService";
 
 const App = () => {
   const petTypes = ["All", "Cat", "Dog", "Other"];
-  const [pets] = useState(dataSource);
-  const [filteredPets, setFilteredPets] = useState(dataSource);
+  const initalPetTabs = {};
+  petTypes.forEach((type, index) => {
+    initalPetTabs[index] = type === "All" ? dataSource.AllPets : [];
+  });
+
+  const [pets] = useState(dataSource.AllPets);
+  const [petTabs, setPetTabs] = useState(initalPetTabs);
   const [selectedPetTypeIndex, setSelectedPetTypeIndex] = useState(0);
 
   const getFilteredPets = petTypeFilter =>
     petTypeFilter && petTypeFilter === "All"
       ? pets
-      : pets.filter(pet => petFilter(pet.species, petTypeFilter));
+      : pets.filter(pet => petFilter(pet.Species, petTypeFilter));
 
   useEffect(() => {
     const petType = petTypes[selectedPetTypeIndex];
     const filteredPets = getFilteredPets(petType);
-    setFilteredPets(filteredPets);
+    const updatedPetTabs = Object.assign({}, petTabs);
+    updatedPetTabs[selectedPetTypeIndex] = filteredPets;
+
+    setPetTabs(updatedPetTabs);
   }, [selectedPetTypeIndex]);
 
-  
   return (
     <div className="pets-app">
       <Tabs onChange={setSelectedPetTypeIndex}>
@@ -36,7 +43,7 @@ const App = () => {
             <TabPanel key={petType}>
               <div className="profile-card-list">
                 <List
-                  dataSource={filteredPets}
+                  dataSource={petTabs[selectedPetTypeIndex]}
                   renderItem={pet => <ProfileCard key={pet.id} {...pet} />}
                 />
               </div>
