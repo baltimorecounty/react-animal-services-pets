9cbe26b6e1ea6396842926c66f280e6db8dc7429
martypowell <jpowel25@gmail.com>
2019-03-26 13:53:50 -0400

    added new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 /** Generic Components - these should be moved to a design system */
-export { default as List } from "./List";
-export { default as ProfileCard } from "./ProfileCard";
+export { default as List } from './List';
+export { default as ProfileCard } from './ProfileCard';
 
 /** App Specific Components */
-export { default as PetTabs } from "./PetTabs";
+export { default as PetDetails } from './PetDetails';
+export { default as PetPage } from './PetPage';
+export { default as PetTabs } from './PetTabs';
