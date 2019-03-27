c35ce992fdb99a8d1be6c2586adba8d2e7df9ab7
Marty Powell <jpowel25@gmail.com>
2019-03-26 21:08:25 -0400

    fixed tests in regards to routing

--- src/components/PetPage.test.js
+++ src/components/PetPage.test.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PetPage from "./PetPage";
 
+const mockProps = {
+  match: {
+    params: {}
+  }
+};
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<PetPage />, div);
+  ReactDOM.render(<PetPage {...mockProps} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
