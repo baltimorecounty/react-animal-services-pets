4d3e3e3ac4a5e0439d4926ee0f687026c6efd906
Marty Powell <jpowel25@gmail.com>
2019-03-26 21:06:32 -0400

    fixed issue with destructuring a null value

--- src/components/PetDetails.js
+++ src/components/PetDetails.js
@@ -4,38 +4,46 @@ import { PetInfoSection as InfoSection } from "./index";
 
 const PetDetails = props => {
   const {
-    AnimalId = "",
-    Name = "",
-    Species = "",
-    Breed = "",
-    Color = "",
-    Sex = "",
-    ShelterArrival = "",
-    AboutMe = "",
-    ImageUrl = ""
-  } = props.pet;
+    AnimalId,
+    Name,
+    Species,
+    Breed,
+    Color,
+    Sex,
+    ShelterArrival,
+    AboutMe,
+    ImageUrl
+  } = props.pet || {};
 
   return (
-    <div id={AnimalId} className="row pet">
-      <div className="col-md-7 col-sm-8">
-        <h2>
-          <Link to={`/${Species}/${AnimalId}`}>{Name}</Link>
-        </h2>
-        <InfoSection label="Animal ID" value={AnimalId} />
-        <InfoSection label="Species" value={Species} />
-        <InfoSection label="Breed" value={Breed} />
-        <InfoSection label="Color" value={Color} />
-        <InfoSection label="Sex" value={Sex} />
-        <InfoSection label="Spayed/Neutered" value={"TODO"} />
-        <InfoSection label="Shelter Arrival" value={ShelterArrival} />
-        <InfoSection label="Shelter Arrival" value={ShelterArrival} />
-        <p className="pet-bio">
-          <strong className="pet-bio-label">About Me</strong> {AboutMe}
-        </p>
-      </div>
-      <div className="col-md-5 col-sm-4">
-        <img src={ImageUrl} alt={`This is ${Name}, this ${Sex} is adoptable`} />
-      </div>
+    <div id={AnimalId} className="row">
+      {AnimalId && (
+        <div className="pet">
+          <div className="col-md-7 col-sm-8">
+            <h2>
+              <Link to={`/${Species}/${AnimalId}`}>{Name}</Link>
+            </h2>
+            <InfoSection label="Animal ID" value={AnimalId} />
+            <InfoSection label="Species" value={Species} />
+            <InfoSection label="Breed" value={Breed} />
+            <InfoSection label="Color" value={Color} />
+            <InfoSection label="Sex" value={Sex} />
+            <InfoSection label="Spayed/Neutered" value={"TODO"} />
+            <InfoSection label="Shelter Arrival" value={ShelterArrival} />
+            <InfoSection label="Shelter Arrival" value={ShelterArrival} />
+            <p className="pet-bio">
+              <strong className="pet-bio-label">About Me</strong> {AboutMe}
+            </p>
+          </div>
+          <div className="col-md-5 col-sm-4">
+            <img
+              src={ImageUrl}
+              alt={`This is ${Name}, this ${Sex} is adoptable`}
+            />
+          </div>
+        </div>
+      )}
+      {!AnimalId && <p>No pet information was found.</p>}
     </div>
   );
 };
