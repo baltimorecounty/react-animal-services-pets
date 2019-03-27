d0ca34935c0715742dc7718082207adee6076383
Marty Powell <jpowel25@gmail.com>
2019-03-26 20:44:20 -0400

    removed deleted component from index

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 /** Generic Components - these should be moved to a design system */
-export { default as List } from './List';
-export { default as ProfileCard } from './ProfileCard';
+export { default as List } from "./List";
 
 /** App Specific Components */
-export { default as PetDetails } from './PetDetails';
-export { default as PetPage } from './PetPage';
-export { default as PetTabs } from './PetTabs';
+export { default as PetDetails } from "./PetDetails";
+export { default as PetPage } from "./PetPage";
+export { default as PetTabs } from "./PetTabs";
