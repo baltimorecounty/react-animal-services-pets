9afe1325a77bc5159abed097ef14dbe41ff4d6e2
Marty Powell <jpowel25@gmail.com>
2019-03-31 20:17:52 -0400

    added button components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 /** Generic Components - these should be moved to a design system */
 export { default as List } from "./List";
+export { default as FacebookShareButton } from "./FacebookShareButton";
 
 /** App Specific Components */
+export { default as SharePetButton } from "./SharePetButton";
 export { default as PetDetails } from "./PetDetails";
 export { default as PetInfoSection } from "./PetInfoSection";
 export { default as PetPage } from "./PetPage";
