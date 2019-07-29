2955a050568d379843b25e99a13d3b9722e7372c
martypowell <jpowel25@gmail.com>
2019-07-29 15:03:20 -0400

    fixed issue where url wasn't working

--- src/components/PetTabs.js
+++ src/components/PetTabs.js
@@ -1,85 +1,88 @@
-import React, { useState, useEffect } from "react";
-import { List, PetDetails } from "./index";
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
-import { getPets } from "../services/PetService";
-import classnames from "classnames";
+import React, { useState, useEffect } from 'react';
+import { List, PetDetails } from './index';
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
+import { getPets } from '../services/PetService';
+import classnames from 'classnames';
 
-const petTypes = ["All", "Cat", "Dog", "Other"];
+const petTypes = [ 'All', 'Cat', 'Dog', 'Other' ];
 
-const PetTabs = props => {
-  const [petTabs, setPetTabs] = useState({});
-  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-  const [selectedTab, setSelectedTab] = useState("all");
+const getPetTypeFromRoute = (routePetType) => {
+	if (routePetType) {
+		const defaultIndex = petTypes.findIndex((petType) => routePetType.toLowerCase() === petType.toLowerCase());
+		const hasRouteTabMatch = defaultIndex !== -1;
 
-  useEffect(() => {
-    const routePetType = props.match.params.petType;
-    if (routePetType) {
-      const defaultIndex = petTypes.findIndex(
-        petType => routePetType.toLowerCase() === petType.toLowerCase()
-      );
-      const hasRouteTabMatch = defaultIndex !== -1;
+		if (hasRouteTabMatch) {
+			const initialSelectedTab = petTypes[defaultIndex].toLowerCase();
 
-      if (hasRouteTabMatch) {
-        const initialSelectedTab = petTypes[defaultIndex].toLowerCase();
+			return {
+				index: defaultIndex,
+				petType: initialSelectedTab
+			};
+		}
+		return {
+			index: 0,
+			petType: 'all'
+		};
+	}
+};
 
-        setSelectedTabIndex(defaultIndex);
-        setSelectedTab(initialSelectedTab);
-      }
-    }
-  });
+const PetTabs = (props) => {
+	const { petType: routePetType } = props.match.params || {};
+	const { index: selectedTabIndex, petType: selectedPetType } = getPetTypeFromRoute(routePetType) || {};
+	const [ petTabs, setPetTabs ] = useState({});
+	const [ selectedTab, setSelectedTab ] = useState(selectedPetType);
 
-  // REMINDER: This useEffect fetches data and should be updated to React Suspense
-  useEffect(() => {
-    let didCancel = false;
+	// REMINDER: This useEffect fetches data and should be updated to React Suspense
+	useEffect(
+		() => {
+			let didCancel = false;
 
-    getPets(selectedTab)
-      .then(pets => {
-        const petTabsData = { ...petTabs };
-        petTabsData[selectedTab] = pets;
+			getPets(selectedTab)
+				.then((pets) => {
+					const petTabsData = { ...petTabs };
+					petTabsData[selectedTab] = pets;
 
-        if (!didCancel) {
-          setPetTabs(petTabsData);
-        }
-      })
-      .catch(console.error);
+					if (!didCancel) {
+						setPetTabs(petTabsData);
+					}
+				})
+				.catch(console.error);
 
-    return () => {
-      didCancel = true;
-    };
-  }, [selectedTab]);
+			return () => {
+				didCancel = true;
+			};
+		},
+		[ selectedTab ]
+	);
 
-  const handleTabChange = selectedTabIndex => {
-    const petType = petTypes[selectedTabIndex].toLowerCase();
-    props.history.push(`/${petType}`);
-    setSelectedTab(petType);
-  };
+	const handleTabChange = (selectedTabIndex) => {
+		const petType = petTypes[selectedTabIndex].toLowerCase();
+		props.history.push(`/${petType}`);
+		setSelectedTab(petType);
+	};
 
-  return (
-    <Tabs
-      defaultIndex={selectedTabIndex}
-      onChange={handleTabChange}
-      className={classnames("tabs", props.className)}
-    >
-      <TabList className="tab-buttons">
-        {petTypes.map(petType => (
-          <Tab key={petType} className="tab-button">
-            {petType}
-          </Tab>
-        ))}
-      </TabList>
-      <TabPanels>
-        {petTypes.map(petType => (
-          <TabPanel key={`tab-panel-${petType}`} className="tab-panel">
-            <List
-              dataSource={petTabs[selectedTab]}
-			  renderItem={pet => <PetDetails key={pet.AnimalId} pet={pet} />}
-			  itemKey="AnimalId"
-            />
-          </TabPanel>
-        ))}
-      </TabPanels>
-    </Tabs>
-  );
+	return (
+		<Tabs index={selectedTabIndex} onChange={handleTabChange} className={classnames('tabs', props.className)}>
+			<TabList className="tab-buttons">
+				{petTypes.map((petType) => (
+					<Tab key={petType} className="tab-button">
+						{petType}
+					</Tab>
+				))}
+			</TabList>
+			<TabPanels>
+				{petTypes.map((petType) => (
+					<TabPanel key={`tab-panel-${petType}`} className="tab-panel">
+						<List
+							dataSource={petTabs[selectedTab]}
+							renderItem={(pet) => <PetDetails key={pet.AnimalId} pet={pet} />}
+							itemKey="AnimalId"
+						/>
+					</TabPanel>
+				))}
+			</TabPanels>
+		</Tabs>
+	);
 };
 
 export default PetTabs;
