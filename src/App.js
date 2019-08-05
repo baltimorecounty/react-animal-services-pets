ed4a825631d36dbadd68832f020f338c80447da8
martypowell <jpowel25@gmail.com>
2019-08-05 11:11:58 -0400

    adjusted route for ues with pet details

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { PetPage, PetTabs } from './components';
 
 const App = () => (
-    <Router>
-        <div className="pets-app">
-            <Route path="/:petType?" exact component={PetTabs} />
-            <Route path="/:petType/:id" exact component={PetPage} />
-        </div>
-    </Router>
+	<Router>
+		<div className="pets-app">
+			<Route path="/:petType?" exact component={PetTabs} />
+			<Route path="/pets/:id/:status" exact component={PetPage} />
+		</div>
+	</Router>
 );
 
 export default App;
