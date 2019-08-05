62203215bbf7e9a1339658e37cb94341b3a7840d
martypowell <jpowel25@gmail.com>
2019-08-05 11:14:19 -0400

    removed unused code

--- src/services/PetService.js
+++ src/services/PetService.js
@@ -1,24 +1,26 @@
-import data from '../data/pets.json';
+import axios from 'axios';
 
-const getPets = filter =>
-    new Promise((resolve, reject) => {
-        const filteredPets =
-            !filter || filter.toLowerCase() === 'all'
-                ? data.AllPets
-                : data.AllPets.filter(pet => petFilter(pet.Species, filter));
-        resolve(filteredPets);
-    });
+const apiBaseUrl = '//localhost:54727/api/pets';
 
-const getPet = id =>
-    new Promise((resolve, reject) => {
-        resolve(data.AllPets.find(pet => pet.AnimalId === id));
-    });
+const getPets = (petType = 'All', status) =>
+	axios
+		.get(`${apiBaseUrl}`, {
+			params: {
+				petType,
+				status
+			}
+		})
+		.then((response) => response.data.Data)
+		.catch(console.error);
 
-const petFilter = (petType, petTypeFilter) =>
-    petTypeFilter === 'Other'
-        ? ['Cat', 'Dog'].indexOf(petType) === -1
-        : petType &&
-          petTypeFilter &&
-          petTypeFilter.toLowerCase() === petType.toLowerCase();
+const getPet = (id, status) =>
+	axios
+		.get(`${apiBaseUrl}/${id}`, {
+			params: {
+				status
+			}
+		})
+		.then((response) => response.data.Data)
+		.catch(console.error);
 
-export { getPet, getPets, petFilter };
+export { getPet, getPets };
