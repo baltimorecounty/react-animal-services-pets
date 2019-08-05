da2393d74d6952aa137e38424a2ab80b8b980db8
martypowell <jpowel25@gmail.com>
2019-08-05 11:12:51 -0400

    fixed infinite loop

--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { PetDetails } from "./index";
-import { getPet } from "../services/PetService";
+import React, { useEffect, useState } from 'react';
+import { PetDetails } from './index';
+import { getPet } from '../services/PetService';
 
-const PetPage = props => {
-  const [pet, setPet] = useState({});
+const PetPage = (props) => {
+	const { id, status } = props.match.params || {};
+	const [ pet, setPet ] = useState({});
 
-  useEffect(() => {
-    const petId = props.match.params.id || 0;
+	useEffect(
+		() => {
+			if (id) {
+				getPet(id, status).then(setPet);
+			}
+		},
+		[ props.match.params ]
+	);
 
-    if (petId) {
-      getPet(petId).then(setPet);
-    }
-  });
-
-  return (
-    <div className="pets-page">
-      <PetDetails pet={pet} />
-    </div>
-  );
+	return (
+		<div className="pets-page">
+			<PetDetails pet={pet} />
+		</div>
+	);
 };
 
 export default PetPage;
