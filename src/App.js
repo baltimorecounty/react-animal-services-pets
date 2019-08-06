dc24dc5b302e7e15ab79683efbd39e0c6bf6995b
martypowell <jpowel25@gmail.com>
2019-08-06 11:37:47 -0400

    added pet status

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { setConfig } from '@baltimorecounty/javascript-utilities/config';
 import { PetPage, PetTabs } from './components';
 import './App.css';
-
+const animalStatus =
+	window.PetsAnimalStatus ||
+	console.error(
+		'Please specify window.PetsAnimalStatus above the script inclusion. Available Statues are Adoptable, Lost, ReportedLost'
+	);
 const apiBaseRoute = 'api/pets';
 setConfig({
 	local: {
 		apiHost: 'localhost:54727',
-		apiRoot: `//localhost:54727/${apiBaseRoute}`
+		apiRoot: `//localhost:54727/${apiBaseRoute}`,
+		animalStatus
 	},
 	development: {
 		apiHost: 'testservices.baltimorecountymd.gov',
-		apiRoot: `//testservices.baltimorecountymd.gov/${apiBaseRoute}`
+		apiRoot: `//testservices.baltimorecountymd.gov/${apiBaseRoute}`,
+		animalStatus
 	},
 	staging: {
 		apiHost: 'testservices.baltimorecountymd.gov',
-		apiRoot: `//testservices.baltimorecountymd.gov/${apiBaseRoute}`
+		apiRoot: `//testservices.baltimorecountymd.gov/${apiBaseRoute}`,
+		animalStatus
 	},
 	production: {
 		apiHost: 'services.baltimorecountymd.gov',
-		apiRoot: `//services.baltimorecountymd.gov/${apiBaseRoute}`
+		apiRoot: `//services.baltimorecountymd.gov/${apiBaseRoute}`,
+		animalStatus
 	}
 });
 
